ec9f7b45d4f96508556c52dff025dcabd95cc871
tallyhawley <league@WTS5.attlocal.net>
2017-04-15 12:01:07 -0700

    Day 1

--- src/level1/Challenge.java
+++ src/level1/Challenge.java
@@ -12,18 +12,24 @@ public class Challenge extends Head {
 	public Challenge(Roomba roomba) {
 		super(roomba);
 	}
+
 	/**
-	 * Without touching the wall reach the endZone
-	 * use sleep an driveDirect 
+	 * Without touching the wall reach the endZone use sleep an driveDirect
 	 */
 	public void initialize() {
-		//write code here!
-		
-		
-		
+		// write code here!
+		driveDirect(1000, 1000);
+		sleep(1300);
+		driveDirect(-375, 375);
+		sleep(500);
+		driveDirect(1000, 1000);
+		sleep(4000);
+		driveDirect(-375, 375);
+		sleep(400);
+		driveDirect(1000, 1000);
 	}
 
 	public void loop() {
-	
+
 	}
 }
